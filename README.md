394fbe974bc17932c9c0ce8a265eb0669a50ad15
Shukri Shukriev <shukri@shukriev.eu>
2015-05-06 12:38:02 +0300

    updated MD file

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 smt_puttingdivsonscreen
 ====================
 
-One of the examples we provide with the toolkit probably the first you will try or copy to get started
+Here is little example that show us how we can put divs on screen's
 
-1) Check out smt_helloworldapp in Eclipse
-2) Build a war using the 'deploy-war' task with the provided build.xml
-3) Do not deploy it in tomcat!, we use the war format but it should be loaded in Lou so use the application manager
+// We set some HTML content on the screen in a div with ID "hello"
+s.setContent("hello", "<div style=\" border: 1px solid #000; width: 200px; height: 50px;\" id=\"divinput\">");
+    	
+//We set some value in our new div which is with ID "divinput"
+s.setContent("divinput", "Hello i'm div");
+
--- src/org/springfield/lou/application/types/PuttingdivsonscreenApplication.java
+++ src/org/springfield/lou/application/types/PuttingdivsonscreenApplication.java
@@ -1,23 +1,23 @@
 /* 
-* HelloworldApplication.java
+* PuttingdivsonscreenApplication.java
 * 
 * Copyright (c) 2012 Noterik B.V.
 * 
 * This file is part of Lou, related to the Noterik Springfield project.
 * It was created as a example of how to use the multiscreen toolkit
 *
-* Helloworld app is free software: you can redistribute it and/or modify
+* Putting divs on screen app is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* Helloworld app is distributed in the hope that it will be useful,
+*  Putting divs on screen  is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with Helloworld app.  If not, see <http://www.gnu.org/licenses/>.
+* along with Putting divs on screen .  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.springfield.lou.application.types;
 import java.util.ArrayList;
